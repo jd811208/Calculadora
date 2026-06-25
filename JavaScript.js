1234add6d937dc772529b035859b37684fdd751a
jd811208 <jonas.dutra.lopes@escola.pr.gov.br>
2026-06-25 10:23:27 -0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -16,5 +16,13 @@ function back() {
 }
 
 function calcular () {
+    var resultado = document.getElementById ('resultado'). innerHTML;
+
+    if ('resultado') {
+        document.getElementById ('resultado').innerHTML = eval ('resultado')
+    }
+    else {
+        document.getElementById ('resultado').innerHTML = "Nada para calcular"
+    }
 
 }
